c6b42ae6514b343b71e40538889449c7dc289dfb
georapbox <georapbox@gmail.com>
2016-01-03 16:27:41 +0200

    Build process

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    header = require('gulp-header'),
+    pkg = require('./package.json');
+
+var banner = [
+    '/**',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @author <%= pkg.author %>',
+    ' * @homepage <%= pkg.homepage %>',
+    ' * @repository <%= pkg.repository.url %>',
+    ' */',
+    ''
+].join('\n');
+
+gulp.task('minify', function () {
+    return gulp.src('src/webStorage.js').
+        pipe(uglify()).
+        pipe(rename({
+            extname: '.min.js'
+        })).
+        pipe(header(banner, {
+            pkg: pkg
+        })).
+        pipe(gulp.dest('dist'));
+});
