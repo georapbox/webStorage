05a1b14ca28ca5ff5b2a89aea02e2d9c944fa4cc
georapbox <georapbox@gmail.com>
2016-01-03 21:17:09 +0200

    Lint on build

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,47 @@
-var gulp = require('gulp'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    header = require('gulp-header'),
-    pkg = require('./package.json');
+(function () {
+    'use strict';
 
-var banner = [
-    '/**',
-    ' * <%= pkg.name %> - <%= pkg.description %>',
-    ' * @version v<%= pkg.version %>',
-    ' * @author <%= pkg.author %>',
-    ' * @homepage <%= pkg.homepage %>',
-    ' * @repository <%= pkg.repository.url %>',
-    ' */',
-    ''
-].join('\n');
+    var gulp = require('gulp'),
+        rename = require('gulp-rename'),
+        uglify = require('gulp-uglify'),
+        header = require('gulp-header'),
+        jshint = require('gulp-jshint'),
+        pkg = require('./package.json');
 
-gulp.task('copy', function () {
-    return gulp.src('src/webStorage.js').
-        pipe(header(banner, {
-            pkg: pkg
-        })).
-        pipe(gulp.dest('dist'));
-});
+    var banner = [
+        '/**',
+        ' * <%= pkg.name %> - <%= pkg.description %>',
+        ' * @version v<%= pkg.version %>',
+        ' * @author <%= pkg.author %>',
+        ' * @homepage <%= pkg.homepage %>',
+        ' * @repository <%= pkg.repository.url %>',
+        ' */',
+        ''
+    ].join('\n');
 
-gulp.task('minify', function () {
-    return gulp.src('src/webStorage.js').
-        pipe(uglify()).
-        pipe(rename({
-            extname: '.min.js'
-        })).
-        pipe(header(banner, {
-            pkg: pkg
-        })).
-        pipe(gulp.dest('dist'));
-});
+    gulp.task('copy', function () {
+        return gulp.src('src/webStorage.js')
+            .pipe(header(banner, {
+                pkg: pkg
+            }))
+            .pipe(gulp.dest('dist'));
+    });
+
+    gulp.task('minify', function () {
+        return gulp.src('src/webStorage.js')
+            .pipe(uglify())
+            .pipe(rename({
+                extname: '.min.js'
+            }))
+            .pipe(header(banner, {
+                pkg: pkg
+            }))
+            .pipe(gulp.dest('dist'));
+    });
+
+    gulp.task('lint', function () {
+        return gulp.src('./dist/webStorage.js')
+            .pipe(jshint())
+            .pipe(jshint.reporter('jshint-stylish'));
+    });
+}());
