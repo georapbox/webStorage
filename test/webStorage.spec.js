9bcc0a63de4393054758e1f5a1caba39e55cdf9d
George Raptis <georapbox@gmail.com>
2017-09-21 21:06:23 +0300

    add more tests

--- test/webStorage.spec.js
+++ test/webStorage.spec.js
@@ -2,25 +2,41 @@
 /* eslint-env jasmine */
 
 describe('webStorage', function () {
-  /* Save and eetrieve items */
-  it('Should succesfully save and retrieve values to localStorage', function () {
-    localStorage.clear();
-    webStorage.setItem('local.obj', {foo: 'bar'});
-    webStorage.setItem('local.str', 'bar');
-    expect(webStorage.getItem('local.obj').foo).toEqual('bar');
-    expect(webStorage.getItem('local.str')).toEqual('bar');
-    localStorage.clear();
+  it('Should create a new instance', function () {
+    const ls = webStorage.createInstance();
+
+    expect(ls.constructor.name).toEqual('WebStorage');
   });
 
-  /* Remove item */
+  it('Should create a new instance with a new namespace', function () {
+    const ls = webStorage.createInstance({
+      name: 'MyApp'
+    });
+
+    expect(ls.storeKeyPrefix).toEqual('MyApp/');
+  });
+
+  it('Should succesfully save and retrieve values to localStorage', function () {
+    localStorage.clear();
+
+    webStorage.setItem('local.obj', {foo: 'bar'});
+
+    webStorage.setItem('local.str', 'bar');
+
+    expect(webStorage.getItem('local.obj').foo).toEqual('bar');
+
+    expect(webStorage.getItem('local.str')).toEqual('bar');
+  });
+
   it('Should remove a saved item by its key', function () {
     localStorage.clear();
+
     webStorage.setItem('local.str', 'bar');
     webStorage.removeItem('local.str');
+
     expect(webStorage.getItem('local.str')).toEqual(null);
   });
 
-  /* Clear */
   it('Should clear all keys from a specific databse', function () {
     localStorage.clear();
 
@@ -35,22 +51,10 @@ describe('webStorage', function () {
     ls.clear();
 
     expect(webStorage.length()).toEqual(2);
+
     expect(ls.length()).toEqual(0);
   });
 
-  /* New Instance */
-  it('Should create a new instance with configured options', function () {
-    localStorage.clear();
-
-    const ls = webStorage.createInstance({
-      driver: sessionStorage,
-      name: 'MyApp'
-    });
-
-    expect(ls.storeKeyPrefix).toEqual('MyApp/');
-  });
-
-  /* Iterate */
   it('Should iterate over all value/key pairs in datastore', function () {
     localStorage.clear();
 
@@ -62,9 +66,9 @@ describe('webStorage', function () {
 
     // Create another instance to test that iterator only works for specific instance database.
     const ls = webStorage.createInstance({
-      driver: sessionStorage,
       name: 'TestApp'
     });
+
     ls.setItem('test1', 'Item 1');
     ls.setItem('test2', 'Item 2');
 
@@ -75,5 +79,48 @@ describe('webStorage', function () {
     });
 
     expect(valuesArr.length).toEqual(5);
+  });
+
+  it('Should return the keys of a datastore as an array of strings', function () {
+    localStorage.clear();
+
+    const ls = webStorage.createInstance({
+      name: 'TestApp'
+    });
+
+    ls.setItem('test1', 'Item 1');
+    ls.setItem('test2', 'Item 2');
+    ls.setItem('test3', 'Item 3');
+
+    const keys = ls.keys();
+
+    expect(keys instanceof Array);
+    expect(keys.length).toBe(3);
+    expect(keys.indexOf('test1')).toBeGreaterThan(-1);
+    expect(keys.indexOf('test2')).toBeGreaterThan(-1);
+    expect(keys.indexOf('test3')).toBeGreaterThan(-1);
+    expect(keys.indexOf('test4')).toBe(-1);
+  });
+
+  it('Should return the length of saved items for a specific database', function () {
+    localStorage.clear();
+
+    const ls1 = webStorage.createInstance({
+      name: 'App1'
+    });
+
+    const ls2 = webStorage.createInstance({
+      name: 'App2'
+    });
+
+    ls1.setItem('test1', 'Item 1');
+    ls1.setItem('test2', 'Item 2');
+    ls1.setItem('test3', 'Item 3');
+
+    ls2.setItem('test1', 'Item 1');
+    ls2.setItem('test2', 'Item 2');
+
+    expect(ls1.length()).toBe(3);
+    expect(ls2.length()).toBe(2);
   });
 });
